2dbd59ad01dc0c3aa13c7801c5af135b130feae7
Anisa J <anisajelitachan@gmail.com>
2024-03-27 09:03:16 +0700

    datatable integration

--- wwwroot/assets/datatables/datatables.js
+++ wwwroot/assets/datatables/datatables.js
@@ -4,7 +4,7 @@
  *
  * To rebuild or modify this file with the latest versions of the included
  * software please visit:
- *   https://datatables.net/download/#bs4/dt-2.0.3
+ *   https://datatables.net/download/#dt/dt-2.0.3
  *
  * Included libraries:
  *   DataTables 2.0.3
@@ -563,7 +563,7 @@
 		 *
 		 *  @type string
 		 */
-		build:"bs4/dt-2.0.3",
+		build:"dt/dt-2.0.3",
 	
 	
 		/**
@@ -11715,7 +11715,7 @@
 		 *
 		 *  @type string
 		 */
-		build:"bs4/dt-2.0.3",
+		build:"dt/dt-2.0.3",
 	
 	
 		/**
@@ -13656,8 +13656,8 @@
 }));
 
 
-/*! DataTables Bootstrap 4 integration
- * ©2011-2017 SpryMedia Ltd - datatables.net/license
+/*! DataTables styling integration
+ * ©2018 SpryMedia Ltd - datatables.net/license
  */
 
 (function( factory ){
@@ -13707,99 +13707,6 @@ var DataTable = $.fn.dataTable;
 
 
 
-/**
- * DataTables integration for Bootstrap 4. This requires Bootstrap 4 and
- * DataTables 1.10 or newer.
- *
- * This file sets the defaults and adds options to DataTables to style its
- * controls using Bootstrap. See https://datatables.net/manual/styling/bootstrap
- * for further information.
- */
-
-/* Set the defaults for DataTables initialisation */
-$.extend( true, DataTable.defaults, {
-	renderer: 'bootstrap'
-} );
-
-
-/* Default class modification */
-$.extend( true, DataTable.ext.classes, {
-	container: "dt-container dt-bootstrap4",
-	search: {
-		input: "form-control form-control-sm"
-	},
-	length: {
-		select: "custom-select custom-select-sm form-control form-control-sm"
-	},
-	processing: {
-		container: "dt-processing card"
-	}
-} );
-
-
-/* Bootstrap paging button renderer */
-DataTable.ext.renderer.pagingButton.bootstrap = function (settings, buttonType, content, active, disabled) {
-	var btnClasses = ['dt-paging-button', 'page-item'];
-
-	if (active) {
-		btnClasses.push('active');
-	}
-
-	if (disabled) {
-		btnClasses.push('disabled')
-	}
-
-	var li = $('<li>').addClass(btnClasses.join(' '));
-	var a = $('<a>', {
-		'href': disabled ? null : '#',
-		'class': 'page-link'
-	})
-		.html(content)
-		.appendTo(li);
-
-	return {
-		display: li,
-		clicker: a
-	};
-};
-
-DataTable.ext.renderer.pagingContainer.bootstrap = function (settings, buttonEls) {
-	return $('<ul/>').addClass('pagination').append(buttonEls);
-};
-
-DataTable.ext.renderer.layout.bootstrap = function ( settings, container, items ) {
-	var row = $( '<div/>', {
-			"class": items.full ?
-				'row justify-content-md-center' :
-				'row justify-content-between'
-		} )
-		.appendTo( container );
-
-	$.each( items, function (key, val) {
-		var klass;
-
-		// Apply left / right margins
-		if (val.table) {
-			klass = 'col-12';
-		}
-		else if (key === 'start') {
-			klass = 'col-md-auto mr-auto';
-		}
-		else if (key === 'end') {
-			klass = 'col-md-auto ml-auto';
-		}
-		else {
-			klass = 'col-md';
-		}
-
-		$( '<div/>', {
-				id: val.id || null,
-				"class": klass+' '+(val.className || '')
-			} )
-			.append( val.contents )
-			.appendTo( row );
-	} );
-};
 
 
 return DataTable;
